f57581d87d697b0da68e51681ad6c8fd4eee3f5f
Akansha <36146495+ak-b@users.noreply.github.com>
2020-09-26 10:17:50 +0530

    Create regex_expressions.py

---
+++ regex_expressions.py
@@ -0,0 +1,23 @@
+'''
+A good example:
+* The first part can contain:
+    * Upper A-Z and lowercase letters a-z
+    * Numbers
+    * Characters: !, #, %, &, *, $, .
+* Must have @
+* Domain:
+    * Can contain any word characters
+    * But only .com ending is allowed
+'''
+
+# Write a regex to match a valid email address
+regex = r"[A-Za-z0-9!#%&*\$\.]+@\w+\.com"
+
+'''
+another example
+* It can contain lowercase a-z and uppercase letters A-Z
+* It can contain numbers
+* It can contain the symbols: *, #, $, %, !, &, .
+* It must be at least 8 characters long but not more than 20
+'''
+regex = r"[a-zA-Z0-9*#\$%\!&\.]{8,20}"
